746f2a40a97e527c25f08aa2eca3d372767ee982
Alex Krueger <alexckrueger@gmail.com>
2022-02-21 18:21:22 -0600

    adds leaflet functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,18 @@ export default {
   data: function () {
     return {
       message: "This is my mapp app",
-      map: "",
+      locations: [
+        { name: "Zazas Pizzeria", latitude: 41.93742, longitude: -87.6483 },
+        { name: "Gordon Ramsay Burger", latitude: 41.89339, longitude: -87.628 },
+        { name: "Friends Station", latitude: 41.89686075995737, longitude: -87.62836438435944 },
+        { name: "Little Wok Wicker Park", latitude: 41.90351, longitude: -87.6768 },
+        { name: "Peanut Park Trattoria", latitude: 41.86911, longitude: -87.66147 },
+      ],
     };
   },
   created: function () {},
-  methods: {},
   mounted: function () {
-    this.map = L.map("map").setView([41.8781, -87.6298], 13);
+    var map = L.map("map").setView([41.8781, -87.6298], 13);
     L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution:
         'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -19,7 +24,25 @@ export default {
       tileSize: 512,
       zoomOffset: -1,
       accessToken: process.env.VUE_APP_MAP_API,
-    }).addTo(this.map);
+    }).addTo(map);
+    var marker = L.marker([41.8781, -87.6298]).addTo(map);
+    marker.bindPopup("<b>Hello world!</b><br>or just Chicago!<br>This is the initial center.");
+    var popup = L.popup();
+
+    function onMapClick(e) {
+      popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+      map.setView(e.latlng);
+    }
+
+    map.on("click", onMapClick);
+
+    this.locations.forEach((location) => {
+      var marker = L.marker([location.latitude, location.longitude]).addTo(map);
+      marker.bindPopup(location.name);
+    });
   },
 };
 </script>
@@ -27,6 +50,9 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <p>There are many like it, but this one is mine.</p>
+    <br />
+    <br />
     <div id="map"></div>
   </div>
 </template>
